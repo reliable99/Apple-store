a10380aabf77f2249b12cf2ce3ffd9711c120854
reliable99 <abdulmuhminsodiq@gmail.com>
2025-07-22 16:13:10 +0100

    zoom functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Control from './components/Control'
+
 
 const App = () => {
+  const [frameZoom, setFramezoom] = useState(false)
+
+  const toggleZoom = () => {
+    setFramezoom(!frameZoom)
+  }
   return (
-    <div>
+    
           <div className="w-full h-screen grid place-items-center">
-            <div className='w-[700vw] h-[85vh] min-w-[70vh] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto'></div>
+            <div className={`${
+          frameZoom && 'min-w-[97vw] min-h-[97vh]'
+        } w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100 flex`}>
+              <Control toggleZoom={toggleZoom} frameZoom={frameZoom} />
+            </div>
           </div>
-    </div>
+    
   )
 }
 
