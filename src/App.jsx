f94a390ee9a938e09267519dacf195e5ea515a70
reliable99 <abdulmuhminsodiq@gmail.com>
2025-07-22 15:26:38 +0100

    setting up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+          <div className="w-full h-screen grid place-items-center">
+            <div className='w-[700vw] h-[85vh] min-w-[70vh] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto'></div>
+          </div>
+    </div>
+  )
+}
+
+export default App
